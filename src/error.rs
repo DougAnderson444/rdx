e1d373b537db1731e5be785068f16edfb2eb0a14
Doug Anderson444 <douganderson444@gmail.com>
2024-11-02 11:23:57 -0300

    add error module

---
+++ src/error.rs
@@ -0,0 +1,10 @@
+use crate::pest::Rule;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Parse error: {0}")]
+    Parse(#[from] Box<pest::error::Error<Rule>>),
+}
