16bb6c261a95f4861dcc8a3b9e291fa66d49cea5
Doug Anderson444 <douganderson444@gmail.com>
2024-11-30 14:05:55 -0400

    allow static mut refs warnings
    it's breaking the CI build because of dirs warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all)]
+#![allow(static_mut_refs)] // dirs crate has warnings that break the CI build.
 
-// The build file results
+// The build file results, builtin components, are included in the build script
 // Build files are for the demos, so the wasm binaries can be included by default in the build
 include!(concat!(env!("OUT_DIR"), "/builtin_components.rs"));
 
