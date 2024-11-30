cd989de653acb86a8eb6b822bfb531a5b35a60e0
Doug Anderson444 <douganderson444@gmail.com>
2024-11-30 13:43:16 -0400

    export build script

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,8 @@ mod layer;
 mod futures;
 mod template;
 mod utils;
+
+/// A module to hold the build script
+mod build_script;
+/// Export helper function to build the script
+pub use build_script::build_script;
