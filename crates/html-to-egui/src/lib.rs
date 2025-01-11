92c371efb7328daba5149575e5224b690f38e954
Doug Anderson444 <douganderson444@gmail.com>
2025-01-11 14:30:56 -0400

    add rustdoc comments for hteg crate

--- crates/html-to-egui/src/lib.rs
+++ crates/html-to-egui/src/lib.rs
@@ -1,7 +1,18 @@
-//! Provides a NewType wrapper around [html::text_content::Division] to enforce
+//! Provides a NewType wrapper around [html] crate to enforce
 //! the use of [Action] for actions, and [Handler] for functions.
 //!
-//! All other [html] methods ar passed through.
+//! This gives us type safety when defining actions and functions,
+//! making parsing the html to egui more robust.
+//!
+//! All other [html] methods ar passed through and can be called also.
+//!
+//! [html] NewType code is generated in the [codegen] module using a macro.
+//!
+//! Other modules include [selectors] for css selectors,
+//! [attribute] for html attributes, and [action] for actions.
+//! The [handler] module provides a builder for [Handler],
+//! which is used to define functions for actions.
+
 #![recursion_limit = "512"] // needed for html crate
 #![allow(missing_docs)]
 #![allow(unused_imports)]
